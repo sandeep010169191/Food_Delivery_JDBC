486aebed06ce0be6c1bdf63fc2f7a0d20c52241c
sandeep010169191 <128699631+sandeep010169191@users.noreply.github.com>
2023-05-10 17:57:05 +0530

    Update food_delivery_sql.sql

--- food_delivery_sql.sql
+++ food_delivery_sql.sql
@@ -1,21 +1,14 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2709
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+create database food_delivery_platform;
+use food_delivery_platform;
 
-\f0\fs24 \cf0 create database food_delivery_platform;\
-use food_delivery_platform;\
-\
-create table restaurants_kolkata(\
-	Rest_id int(2) not null auto_increment,\
-    Rest_name varchar(50),\
-    Rest_contact int(10),\
-    Rest_location varchar(50),\
-    Rest_hours varchar(20),\
-    primary key(Rest_id)\
-);\
+create table restaurants_kolkata(
+	Rest_id int(2) not null auto_increment,
+    Rest_name varchar(50),
+    Rest_contact int(10),
+    Rest_location varchar(50),
+    Rest_hours varchar(20),
+    primary key(Rest_id)
+);
 \
 create table restaurants_delhi(\
 	Rest_id int(2) not null auto_increment,\
